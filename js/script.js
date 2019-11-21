0fbfb6f931e01c835ed847e41fcaaa7d5ddca6de
matuzalema <matuzalema@gmail.com>
2019-11-21 02:57:25 +0100

    Refactoring code

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
-var inputElement = document.getElementById('files');
-var fileData = document.getElementById('file-data');
-var imgList = document.getElementById('img-list');
+const inputElement = document.getElementById('files');
+const fileData = document.getElementById('file-data');
+const imgList = document.getElementById('img-list');
+let name;
+let size;
+let stored;
+const dataImg = {
+    name: name,
+    size: size
+}
+
+inputElement.addEventListener('change', handleFiles);
 
 function handleFiles(event) {
-    var fileList = event.target.files;
+    event.stopPropagation();
+    event.preventDefault();
+    const fileList = event.target.files;
 
-    for(var i=0; i<fileList.length; i++){
-        var file = fileList[i];
-        var dataURL;
-        var longitude;
-        var latitude;
+    for (let i = 0; i < fileList.length; i++) {
+        const file = fileList[i];
 
-        //GPS
-        var toDecimal = function (number) {
+        // GPS
+        const toDecimal = function (number) {
             return number[0].numerator + number[1].numerator / (60 * number[1].denominator) + number[2].numerator / (3600 * number[2].denominator);
         };
 
@@ -20,58 +28,39 @@ function handleFiles(event) {
             EXIF.getData(image, function () {
                 longitude = toDecimal(EXIF.getTag(this, 'GPSLongitude'));
                 latitude = toDecimal(EXIF.getTag(this, 'GPSLatitude'));
+
+                dataImg.longitude = longitude;
+                dataImg.latitude = latitude;
             });
         })(file);
-        
-        
-        var reader = new FileReader();
-        reader.onload = function(e) {
 
-            //check if file is smaller then 1MB and chceck if file has got GPS
-            if (file.size > 8388608 ){
-                return alert('To large file. The file cannot be greater then 1MB.');
-            } else if (longitude === undefined || latitude === undefined){
-                return alert("This file hasnt got a GPS");
-            }
+        //FileReader
+        const reader = new FileReader();
+        reader.onload = (function (file) {
+            return function(e){
+                const dataURL = e.target.result;
+                dataImg.name = file.name;
+                dataImg.size = file.size;
+                dataImg.src = dataURL;
 
-            //Add file list
-            dataURL = e.target.result;
+                // localStorage
+                sessionStorage.setItem(file.name, JSON.stringify(dataImg));
+                const retrivedStorage = sessionStorage.getItem(dataImg.name);
+                stored = JSON.parse(retrivedStorage);
 
-            fileData.insertAdjacentHTML('afterbegin', '<ul id="img-list"></ul>');
-            var imgList = document.getElementById('img-list');
-            
-            imgList.insertAdjacentHTML('afterbegin', '<button class="remove-btn">Remove</button>');
-            imgList.insertAdjacentHTML('afterbegin', '<li class="img-data file-size"> longitude:' + longitude + ' </li>');
-            imgList.insertAdjacentHTML('afterbegin', '<li class="img-data file-size"> latitude:' + latitude + '</li>');
-            imgList.insertAdjacentHTML('afterbegin', '<li class="img-data file-size"> size: ' + file.size + ' bites </li>');
-            imgList.insertAdjacentHTML('afterbegin', '<li class="img-data file-name"> name: ' + file.name + '</li>');
-            imgList.insertAdjacentHTML('afterbegin', '<img width=100 src=' + dataURL + '>');
-            
-            //button remove
-            var removeBtn = document.querySelectorAll('.remove-btn');
-            for(var i=0; i<removeBtn.length; i++){
-                removeBtn[i].addEventListener('click', function(e){
-                    e.stopPropagation();
-                    e.preventDefault();
-                    this.parentNode.remove();
-                })
-            }
+                //HTML
+                fileData.insertAdjacentHTML('afterbegin', '<ul id="img-list"></ul>');
+                const imgList = document.getElementById('img-list');
+                imgList.insertAdjacentHTML('afterbegin', '<li class="img-data file-size"> latitudde: ' + stored.latitude + ' </li>');
 
-            //localStorage
-            const dataImg = {
-                latitude: latitude,
-                longitude: longitude,
-                size: file.size,
-                src: dataURL
+                imgList.insertAdjacentHTML('afterbegin', '<li class="img-data file-size"> longitude: ' + stored.longitude + ' </li>');
+
+                imgList.insertAdjacentHTML('afterbegin', '<li class="img-data file-size"> size: ' + stored.size + ' bites </li>');
+                imgList.insertAdjacentHTML('afterbegin', '<li class="img-data file-size"> name: ' + stored.name + '</li>');
+                imgList.insertAdjacentHTML('afterbegin', '<img width=100 src=' + stored.src  + '>');
             };
-
-            localStorage.clear();
-            sessionStorage.setItem(file.name, JSON.stringify(dataImg));
-        };
-        reader.readAsDataURL(file);  
+        })(file);
+        reader.readAsDataURL(file);
     }
 }
-
 inputElement.addEventListener('change', handleFiles);
-
-
